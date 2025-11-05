ff79d20ce9a5f0d9b8b4b3a925dd4221035d1588
juliettehnr <juliette.hnr.pro@gmail.com>
2025-11-05 14:28:12 +0100

    ajout argparse + suppression des pays

--- clean-data.py
+++ clean-data.py
@@ -7,26 +7,48 @@ Script de nettoyage et de préparation du corpus pour le classifieur.
 
 import pandas as pd
 import re
+import argparse
 
 # Suppresion des balises
 def nettoyer_html(texte):
-    return re.sub(r"<[^>]*>", "", str(texte))
 
-# Paramètres
-fichier_entree = "tableB1.csv"
-fichier_sortie = "cleanB1.csv"
-colonne_texte = "Texte"
-colonne_langue = "Langue"
-langues_a_supprimer = ["KABYLE", "ESPAGNOL", "JAPONAIS"]
+    texte = re.sub(r"<[^>]*>", "", str(texte))
+    texte = re.sub(r"\b(C|c)hin(e|ois)\b", "", str(texte))
+    texte = re.sub(r"\b(A|a)ngl(eterre|ais)\b", "", str(texte))
+    texte = re.sub(r"\b(E|e)spagn(e|ol)\b", "", str(texte))
+    texte = re.sub(r"\b(P|p)ortuga(l|is)\b", "", str(texte))
+    texte = re.sub(r"\b(R|r)uss(ie|e)\b", "", str(texte))
+    texte = re.sub(r"\b(J|j)apon(ais)?\b", "", str(texte))
+    texte = re.sub(r"\b(K|k)abyle\b", "", str(texte))
+    texte = re.sub(r"\b(A|a)rabe\b", "", str(texte))
 
-# Lecture et nettoyage
-df = pd.read_csv(fichier_entree, encoding="utf-8")
-df[colonne_texte] = df[colonne_texte].apply(nettoyer_html)
-df = df[~df[colonne_langue].isin(langues_a_supprimer)]
-
-# Sauvegarde
-df.to_csv(fichier_sortie, index=False, encoding="utf-8")
+    return texte
 
 
+def main():
+
+    # 1. Créer un parser pour entrer le nom du fichier à traiter
+    parser = argparse.ArgumentParser(description="Choix d'une table CSV")
+    parser.add_argument("-i", "--fichierEntree", help="Entrez le nom du fichier csv pour l'input", required=True)
+    parser.add_argument("-o", "--fichierSortie", help="Entrez le nom du fichier csv pour l'output", required=True)
+    args = parser.parse_args()
+
+    # 2.Paramètres
+    colonne_texte = "Texte"
+    colonne_langue = "Langue"
+    fichier_entree = args.fichierEntree
+    fichier_sortie = args.fichierSortie
+    langues_a_supprimer = ["KABYLE", "ESPAGNOL", "JAPONAIS", "ANGLAIS", "RUSSE"]
+
+    # 3.Lecture et nettoyage
+    df = pd.read_csv(fichier_entree, encoding="utf-8")
+    df[colonne_texte] = df[colonne_texte].apply(nettoyer_html)
+    df = df[~df[colonne_langue].isin(langues_a_supprimer)]
+
+    # 4.Sauvegarde
+    df.to_csv(fichier_sortie, index=False, encoding="utf-8")
 
 
+
+if __name__ == "__main__":
+    main()
