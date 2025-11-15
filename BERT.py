caed31fdbf72cb0e1209721ec201fde08f14cf38
Inesmartins1912 <inesmartins1912@gmail.com>
2025-11-15 17:29:23 +0100

    nouvelle petite modiffication

--- BERT.py
+++ BERT.py
@@ -20,7 +20,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, AdamW
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from torch.optim import AdamW
 import torch
 import numpy as np
 import time
